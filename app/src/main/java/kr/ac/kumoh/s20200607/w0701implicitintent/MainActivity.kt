147740a66d047dea3b203b997cfcb51da9ea81af
Jimin <77067383+wl2258@users.noreply.github.com>
2022-10-17 19:46:14 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/s20200607/w0701implicitintent/MainActivity.kt
@@ -0,0 +1,24 @@
+package kr.ac.kumoh.s20200607.w0701implicitintent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.ac.kumoh.s20200607.w0701implicitintent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnHomepage.setOnClickListener {
+            val uri = Uri.parse("https://www.velog.io/ssonzm/")
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+    }
+}
